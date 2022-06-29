35aa825820fd308a06826358c6c3145d8de30762
MandyL2 <fioleewriter@gmail.com>
2022-06-29 15:10:14 -0400

    updated reading responses

--- reading_responses/readings.js
+++ reading_responses/readings.js
@@ -1,8 +1,12 @@
 let readingWindows = ["reading1", "reading2", "reading3", "reading4"];
 let exerciseWindows = ["exercise1", "exercise2", "exercise3", "exercise4", "exercise5", "exercise6"];
+let margin = 20;
 function openWindow(el) {
 	if (document.getElementById(readingWindows[el]) != undefined) {
 		document.getElementById(readingWindows[el]).style.display = "block";
+		document.getElementById(readingWindows[el]).style.marginTop = margin + "px";
+		document.getElementById(readingWindows[el]).style.marginRight = margin + "px";
+		margin += 20;
 	}
 	if (document.getElementById(exerciseWindows[el]) != undefined) {
 		document.getElementById(exerciseWindows[el]).style.display = "block";
@@ -19,3 +23,11 @@ function closeWindow(el) {
 }
 
 $(".reading-outer").draggable();
+let counter = 0;
+$(document).on("mousedown", ".reading-outer", function () {
+	$(this).css("z-index", counter);
+});
+
+$(document).on("mouseup", ".reading-outer", function () {
+	counter++;
+});
